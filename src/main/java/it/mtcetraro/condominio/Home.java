c79e3315b272af453948528b43cd6e3045070cf1
Kovandowski <matteo.cetraro@icloud.com>
2026-07-23 20:05:32 +0200

    Implementazione della logica di Registrazione!

--- src/main/java/it/mtcetraro/condominio/Home.java
+++ src/main/java/it/mtcetraro/condominio/Home.java
@@ -18,4 +18,18 @@ public class Home {
         }
     }
 
+    public boolean Registrazione(String Username, String Password){
+        try (Connection conn = DatabaseManager.getConnection()) {  
+            Register register = new Register();
+            boolean utente_registrato = register.makeRegistration(conn, Username, Password);
+            return utente_registrato;
+
+        } catch (SQLException e) {
+            System.err.println("Errore durante la lettura dal database:");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
 }
