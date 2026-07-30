0fc810ad3b7d766b884bfafb185892736879bd94
Kovandowski <matteo.cetraro@icloud.com>
2026-07-30 15:22:06 +0200

    Implementazione sidebar

--- src/main/java/it/mtcetraro/condominio/DashboardView.java
+++ src/main/java/it/mtcetraro/condominio/DashboardView.java
@@ -17,19 +17,20 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 public class DashboardView extends BorderPane{
-    private Button home;
-    private Button view;
-    private Button costo;
+    private VBox sidebar;
+    private StackPane areaContenuto;
     
     public DashboardView(Stage stage, Condominio condominio){
         this.setPadding(new Insets(20));
@@ -42,13 +43,15 @@ public class DashboardView extends BorderPane{
         areaContenuto.setMinWidth(Double.MAX_VALUE);
         areaContenuto.setStyle("-fx-background-color: #1a87db; -fx-background-radius: 6px");
 
-        this.setLeft(this.creaSidebar());
+        sidebar = this.createSidebar(condominio);
+
+        this.setLeft(sidebar);
         this.setCenter(areaContenuto);
     }
 
-    private VBox creaSidebar(){
+    private VBox createSidebar(Condominio condominio){
         VBox sidebar = new VBox();
-        sidebar.setPadding(new Insets(15));
+        sidebar.setPadding(new Insets(40, 20, 20, 20));
         sidebar.setSpacing(15);
 
         sidebar.setPrefWidth(220);
@@ -56,9 +59,32 @@ public class DashboardView extends BorderPane{
         sidebar.setMaxWidth(220);
         sidebar.setStyle("-fx-background-color: #000000;  -fx-background-radius: 6px");
 
-        
+        String nomeLogo = condominio.getNome();
+        Label logo = new Label(nomeLogo);
+        logo.setStyle("-fx-text-fill: #ffffff; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+        Label menu = new Label("Menu:");
+        menu.setStyle("-fx-text-fill: #64748b; -fx-font-size: 12px; -fx-font-weight: bold;");
+        sidebar.setMargin(menu, new Insets(30, 0, 15, 0));
 
+        sidebar.getChildren().addAll(logo, menu);
+
+        VBox menuBar = new VBox();
+        menuBar.setSpacing(15);
+
+        Button home = new Button("Home");
+        home.setStyle( "-fx-text-size:13; -fx-text-weight: bold; -fx-cursor: hand; -fx-background-color: #c3b05b; -fx-text-fill: #ffffff");
+        home.setPrefWidth(120);
+        home.setPrefHeight(35);
+        SVGPath iconaHome = new SVGPath();
+        iconaHome.setContent("M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z");
+        iconaHome.setStyle("-fx-text-fill: #ffffff;");
+        home.setGraphic(iconaHome);
+        home.setGraphicTextGap(15);
+        home.setAlignment(Pos.CENTER_LEFT);
+        menuBar.getChildren().addAll(home);
+
+        sidebar.getChildren().add(menuBar);
 
         return sidebar;
     }
