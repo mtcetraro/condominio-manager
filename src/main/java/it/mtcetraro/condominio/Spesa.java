2309b97fbef525f5a18795d835ff9131a0b98163
Kovandowski <matteo.cetraro@icloud.com>
2026-08-13 00:05:25 +0200

    Implementazione dell'aggiunta di una spesa

--- src/main/java/it/mtcetraro/condominio/Spesa.java
+++ src/main/java/it/mtcetraro/condominio/Spesa.java
@@ -1,5 +1,8 @@
 package it.mtcetraro.condominio;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
 
 public class Spesa {
     private String fattura;
@@ -18,5 +21,24 @@ public class Spesa {
         this.tabella = tabella;
     }
 
-
+    public boolean inserisci(Connection conn){
+        String query = "INSERT INTO SPESA(NumFattura, Condominio, Tipologia, Cifra, DataPagamento, TabellaMillessimale) VALUES (?, ?, ?, ?, ?, ?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setString(1, this.fattura);
+            pstmt.setString(2, this.condominio);
+            pstmt.setString(3, this.tipologia);
+            pstmt.setDouble(4, this.cifra);
+            pstmt.setDate(5, this.data);
+            pstmt.setString(6, tabella);
+            int rows = pstmt.executeUpdate();
+            if(rows==1){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
