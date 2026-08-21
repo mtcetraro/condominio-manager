4356bd4016236b1eff37402a3a1146615ce3e48a
Kovandowski <matteo.cetraro@icloud.com>
2026-08-21 16:18:34 +0200

    Lavoro su Tabelle

---
+++ src/main/java/it/mtcetraro/condominio/AppartamentoTabella.java
@@ -0,0 +1,22 @@
+package it.mtcetraro.condominio;
+
+public class AppartamentoTabella {
+    private String condominio;
+    private String tabella;
+    private String appartamento;
+    private int millesimi;
+    private double tassa;
+
+    public AppartamentoTabella(String condominio, String tabella, String appartamento, int millesimi, double tassa){
+        this.condominio = condominio;
+        this.tabella = tabella;
+        this.appartamento = appartamento;
+        this.millesimi = millesimi;
+        this.tassa = tassa;
+    }
+
+    @Override
+    public String toString(){
+        return this.appartamento + " - " + this.millesimi + " - " + this.tassa + "€";
+    }
+}
